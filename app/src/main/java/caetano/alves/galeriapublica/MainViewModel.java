15ea315ff29e643b3d516fb2369f3fc736cf08bc
20211TIIMI0060 <gustavo.guguvat@gmail.com>
2023-08-25 11:02:58 -0300

    Dia 4 - Terminei passo 8.4, comecar passo 8.5, ja criei o MyViewHolder, continuar do ListAdapter

--- app/src/main/java/caetano/alves/galeriapublica/MainViewModel.java
+++ app/src/main/java/caetano/alves/galeriapublica/MainViewModel.java
@@ -4,9 +4,19 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelKt;
+import androidx.paging.Pager;
+import androidx.paging.PagingConfig;
+import androidx.paging.PagingData;
+import androidx.paging.PagingLiveData;
+
+import kotlinx.coroutines.CoroutineScope;
 
 // Classe herda de AndroidViewModel para poder acessar o contexto da aplicacao
 public class MainViewModel extends AndroidViewModel {
+
+    LiveData<PagingData<ImageData>> pageLv;
 
     // Inteiro que identifica se o selecionado é grid ou list
     int navigationOpSelected = R.id.gridViewOp;
@@ -14,6 +24,15 @@ public class MainViewModel extends AndroidViewModel {
     // Construtor
     public MainViewModel(@NonNull Application application){
         super(application);
+        GalleryRepository galleryRepository = new GalleryRepository(application);
+        GalleryPagingSource galleryPagingSource = new GalleryPagingSource(galleryRepository);
+        Pager<Integer, ImageData> pager = new Pager(new PagingConfig(10), () -> galleryPagingSource);
+        CoroutineScope viewModelScope = ViewModelKt.getViewModelScope(this);
+        pageLv = PagingLiveData.cachedIn(PagingLiveData.getLiveData(pager), viewModelScope);
+    }
+
+    public LiveData<PagingData<ImageData>> getPageLv() {
+        return pageLv;
     }
 
     // Metodo para pegar o inteiro de identificacao
---
+++ app/src/main/java/caetano/alves/galeriapublica/MyViewHolder.java
@@ -0,0 +1,13 @@
+package caetano.alves.galeriapublica;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+// Classe que armazena uma view para auxiliar no funcionamento do recycler view
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
